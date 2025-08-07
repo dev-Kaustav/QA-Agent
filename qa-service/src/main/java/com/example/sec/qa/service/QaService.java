1c18fc802173de61cd9f4f43b2eab64558e0a27b
Kaustav Mukherjee <job.kaustav@gmail.com>
2025-08-07 20:48:43 +0530

    chore: remove legacy qa service

--- qa-service/src/main/java/com/example/sec/qa/service/QaService.java
+++
@@ -1,8 +0,0 @@
-package com.example.sec.qa.service;
-
-import com.example.sec.qa.model.QaRequest;
-import com.example.sec.qa.model.QaResponse;
-
-public interface QaService {
-  QaResponse answer(QaRequest request);
-}
