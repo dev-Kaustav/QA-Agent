77a65bf7abfae2fd2a6d13e148841183d7639895
Kaustav Mukherjee <job.kaustav@gmail.com>
2025-08-07 16:08:27 +0530

    Add H2 test configuration for embedding service

--- embedding-service/src/test/java/com/example/sec/embedding/EmbeddingServiceApplicationTests.java
+++ embedding-service/src/test/java/com/example/sec/embedding/EmbeddingServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.sec.embedding;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class EmbeddingServiceApplicationTests {
   @Test
   void contextLoads() {}
