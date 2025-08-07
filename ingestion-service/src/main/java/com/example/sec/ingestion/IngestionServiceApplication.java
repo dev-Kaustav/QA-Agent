38b9302dd1722d3faa8f6482708a79f083d27bdb
Kaustav Mukherjee <job.kaustav@gmail.com>
2025-08-07 19:48:26 +0530

    Connect ingestion to chunker and trigger embeddings

--- ingestion-service/src/main/java/com/example/sec/ingestion/IngestionServiceApplication.java
+++ ingestion-service/src/main/java/com/example/sec/ingestion/IngestionServiceApplication.java
@@ -2,18 +2,10 @@ package com.example.sec.ingestion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class IngestionServiceApplication {
-  public static void main(String[] args) {
-    SpringApplication.run(IngestionServiceApplication.class, args);
-  }
-
-  @Bean
-  public RestTemplate restTemplate(RestTemplateBuilder builder) {
-    return builder.build();
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(IngestionServiceApplication.class, args);
+    }
 }
